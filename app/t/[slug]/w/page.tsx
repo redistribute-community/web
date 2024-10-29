c01c205bacae1cc9069f272b461cd15bb0eac17d
redistribute-individual <redistribute.community@proton.me>
2024-10-29 10:45:30 -0400

    fix | scroll for Safari

--- app/t/[slug]/w/page.tsx
+++ app/t/[slug]/w/page.tsx
@@ -12,10 +12,12 @@ export default async function Page({ params }) {
     (await getPerspectives(slug, locked, token?.value)) || [];
 
   return (
-    <main className="flex flex-col items-center min-h-screen h-full">
-      <div className="flex flex-col justify-between w-full h-full overflow-hidden relative">
+    <main className="flex flex-col items-center h-full">
+      <div
+        className={`flex flex-col ${token ? "w-full justify-between" : "justify-end"} h-full`}
+      >
         {!token ? (
-          <div className="flex grow-0">
+          <div className="mb-4">
             <Token topicId={slug} perspectiveId={null} />
           </div>
         ) : (
--- components/Token.tsx
+++ components/Token.tsx
@@ -7,40 +7,36 @@ export function Token({ topicId, perspectiveId }) {
   const [inputToken, setInputToken] = useState(null);
 
   return (
-    <div className="fixed -translate-x-2/4 -translate-y-2/4 top-1/2 left-1/2">
-      <div className="flex grow-0">
-        <div className="flex flex-row items-center">
-          <label className="sr-only" htmlFor="token">
-            token
-          </label>
-          <input
-            onChange={(e) => {
-              setInputToken(e.target.value);
-            }}
-            data-testid="token"
-            className="rounded border border-gray-700 dark:bg-slate-800/20 focus:ring-purple-700 text-xs w-full"
-            type="text"
-            id="token"
-            name="token"
-            placeholder="🔑"
-            autoComplete="off"
-            required
-          />
+    <div className="flex flex-row">
+      <label className="sr-only" htmlFor="token">
+        token
+      </label>
+      <input
+        onChange={(e) => {
+          setInputToken(e.target.value);
+        }}
+        data-testid="token"
+        className="rounded border border-gray-700 dark:bg-slate-800/20 focus:ring-purple-700 text-xs w-full"
+        type="text"
+        id="token"
+        name="token"
+        placeholder="🔑"
+        autoComplete="off"
+        required
+      />
 
-          <button
-            data-testid="save"
-            className="rounded border border-gray-700 dark:bg-slate-800/20 focus:ring-purple-700 text-xs ml-1 p-1"
-            id="save"
-            name="save"
-            type="button"
-            onClick={() => {
-              setCookie(inputToken, topicId, perspectiveId);
-            }}
-          >
-            💾
-          </button>
-        </div>
-      </div>
+      <button
+        data-testid="save"
+        className="rounded border border-gray-700 dark:bg-slate-800/20 focus:ring-purple-700 text-xs ml-1 p-1"
+        id="save"
+        name="save"
+        type="button"
+        onClick={() => {
+          setCookie(inputToken, topicId, perspectiveId);
+        }}
+      >
+        💾
+      </button>
     </div>
   );
 }
